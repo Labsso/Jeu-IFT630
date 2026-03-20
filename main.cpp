4ef31b66b5b130dda3386538bf40cb2dfb6e087a
comh5893 <comh5893@usherbrooke.ca>
2026-03-19 21:03:51 -0400

    Ajout librairie raylib
    Lancer le cmake et ça devrait installer correctement sur linux ajouter ça :
    
    # ---- Linux only (needed for raylib) ----
    if (UNIX AND NOT APPLE)
        target_link_libraries(MyRaylibGame m pthread dl rt X11)
    endif()

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
+#include "raylib.h"
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    InitWindow(800, 600, "raylib + CLion");
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("It works!", 350, 280, 20, BLACK);
+        EndDrawing();
     }
 
+    CloseWindow();
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
